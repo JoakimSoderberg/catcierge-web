6456f7003d5fbd9946fdf46506da63040f4c3d54
Joakim Soderberg <joakim.soderberg@gmail.com>
2014-09-15 22:43:15 +0200

    Add an initial template with vis-js loaded.

--- catcierge-web.py
+++ catcierge-web.py
@@ -1,15 +1,54 @@
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
+import tornado.template
+import tornado.httpserver
+import os
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+from tornado.options import define, options, parse_command_line
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+define("port", default=8888, help="Run server on the given port", type=int)
+
+clients = dict()
+
+class IndexHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
+	def get(self):
+		self.render('index.html')
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+	def open(self, *args):
+		self.id = self.get_argument('Id')
+		self.stream.set_nodelay(True)
+		clients[self.id] = {'id': self.id, 'object': self}
+
+	def on_message(self, message):
+		print "Client %s received a message : %s" % (self.id, message)
+
+	def on_close(self):
+		if self.id in clients:
+			del clients[self.id]
+
+class Application(tornado.web.Application):
+	def __init__(self):
+		handlers = [
+			(r'/', IndexHandler),
+			(r'/', WebSocketHandler),
+		]
+
+		settings = dict(
+			template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+			static_path=os.path.join(os.path.dirname(__file__), 'static'),
+			debug=True,
+			)
+
+		tornado.web.Application.__init__(self, handlers, **settings)
+
+def main():
+	tornado.options.parse_command_line()
+	http_server = tornado.httpserver.HTTPServer(Application())
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-    application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
-
+	main()
